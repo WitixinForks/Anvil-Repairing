cf10c152a9ff65d306a984d67173713a076745df
Tyler Hancock <darklime@live.ca>
2023-07-16 12:22:54 -0600

    Add an advancement for the repairing feature.

--- common/src/main/java/net/darkhax/anvilrepairing/AnvilRepairCommon.java
+++ common/src/main/java/net/darkhax/anvilrepairing/AnvilRepairCommon.java
@@ -1,9 +1,13 @@
 package net.darkhax.anvilrepairing;
 
+import net.minecraft.ResourceLocationException;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -20,6 +24,7 @@ import net.minecraft.world.phys.BlockHitResult;
 public class AnvilRepairCommon {
 
     public static final String MODID = "anvilrepairing";
+    private static final ResourceLocation ADVANCEMENT_ID = new ResourceLocation(MODID, "story/repair");
     public static final TagKey<Item> ANVIL_REPAIR_ITEMS = TagKey.create(Registries.ITEM, new ResourceLocation(MODID, "anvil_repair_items"));
 
     public static InteractionResult attemptAnvilRepair(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
@@ -34,6 +39,11 @@ public class AnvilRepairCommon {
                 if (!player.getAbilities().instabuild) {
 
                     player.getItemInHand(hand).shrink(1);
+                }
+
+                if (player instanceof ServerPlayer serverPlayer) {
+
+                    awardAdvancement(serverPlayer, ADVANCEMENT_ID);
                 }
 
                 repairAnvil(level, pos, blockState.getBlock(), blockState.getValue(AnvilBlock.FACING));
@@ -61,6 +71,29 @@ public class AnvilRepairCommon {
 
             level.setBlock(pos, Blocks.CHIPPED_ANVIL.defaultBlockState().setValue(AnvilBlock.FACING, direction), Block.UPDATE_CLIENTS);
             level.levelEvent(LevelEvent.SOUND_ANVIL_USED, pos, 0);
+        }
+    }
+
+    private static void awardAdvancement(ServerPlayer player, ResourceLocation advancementId) {
+
+        final Advancement toGrant = player.getServer().getAdvancements().getAdvancement(advancementId);
+
+        if (toGrant != null) {
+
+            final AdvancementProgress progress = player.getAdvancements().getOrStartProgress(toGrant);
+
+            if (!progress.isDone()) {
+
+                for(String remainingCriteria : progress.getRemainingCriteria()) {
+
+                    player.getAdvancements().award(toGrant, remainingCriteria);
+                }
+            }
+        }
+
+        else {
+
+            throw new ResourceLocationException("No advancement found for ID: " + advancementId);
         }
     }
 }
